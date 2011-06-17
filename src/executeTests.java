d4aa28457d79964d84c96e425d95c67a64369dfa
Uros Kosic <ukosic@gmail.com>
2011-06-17 19:43:41 +0200

    Measurments done

--- src/executeTests.java
+++ src/executeTests.java
@@ -6,6 +6,15 @@ public class executeTests {
 	
 	public static void main(String[] args) {
 		
+		if(!(args.length > 0)) {
+			System.out.println("Za izbiro algoritma podaj \"genetic\" ali \"hill\" kot argument.\n" +
+			"Pozor: za hill climbing je potrebna inicializirana začetna množica seedov v mapi initPop.");
+			return;
+		}
+		
+
+
+		
 		LinkedList<Warrior> warriors = Specs.readSpecs("specs");
 		
 		Warrior roleModel1 = warriors.get(0);
@@ -17,7 +26,6 @@ public class executeTests {
 		final double ELITE_FACTOR = 0.2;
 		final boolean ALFABETA = true;
 		
-		
 		GeneticOptimization genetic = new GeneticOptimization(
 				roleModel1,
 				roleModel2,
@@ -27,11 +35,23 @@ public class executeTests {
 				MUTATION_FACTOR,
 				ELITE_FACTOR,
 				ALFABETA);
-//		genetic.run();
 		
-		ArrayList<OptimizationAlgorithm.Individual> seeds = Specs.readSpecsForSeeds("initPop//ind", POPULATION_SIZE, genetic);
-		HillClimbing hill = new HillClimbing(warriors.get(0), warriors.get(1), GAME_PER_INDIVIDUAL_REPEAT, ALFABETA, MINMAX_DEPTH, seeds);
-		hill.run();
+		if(args[0].equals("genetic")) {
+			System.out.println("Running genetic optimization...");
+			genetic.run();
+		}
+		else if(args[0].equals("hill")) {
+			System.out.println("Running hill climbing optimization...");
+			ArrayList<OptimizationAlgorithm.Individual> seeds = Specs.readSpecsForSeeds("initPop//ind", POPULATION_SIZE, genetic);
+			HillClimbing hill = new HillClimbing(warriors.get(0), warriors.get(1), GAME_PER_INDIVIDUAL_REPEAT, ALFABETA, MINMAX_DEPTH, seeds);
+			hill.run();
+		}
+		
+		else {
+				System.out.println("Napacen parameter.");
+				return;
+		}
+		
 		
 	
 	}
